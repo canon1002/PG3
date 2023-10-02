5594a79a69b703882ae07ce6ad813bca48362e0e
k022g0038@g.neec.ac.jp <k022g0038@g.neec.ac.jp>
2023-10-02 11:56:38 +0900

    template<T>を使用

--- main.cpp
+++ main.cpp
@@ -1,65 +1,28 @@
 #include <stdio.h>
 
-static int Min(int a, int b) {
+template <typename T>
+T Min(T a, T b) 
+{
 	if (a < b) {
-		printf("%d\n", a);
 		return a;
 	}
-	if (a > b) {
-		printf("%d\n", b);
+	else {
 		return b;
 	}
-	if (a = b) {
-		printf("入力された値が同じです\n");
-		return 0;
-	}
-
-	return 0;
-}
-static float Min(float a, float b) {
-	if (a < b) {
-		printf("%f\n", a);
-		return a;
-	}
-	if (a > b) {
-		printf("%f\n", b);
-		return b;
-	}
-	if (a = b) {
-		printf("入力された値が同じです\n");
-		return 0;
-	}
-
-	return 0;
-}
-static double Min(double a, double b) {
-	if (a < b) {
-		printf("%lf\n", a);
-		return a;
-	}
-	if (a > b) {
-		printf("%lf\n",b);
-		return b;
-	}
-	if (a = b) {
-		printf("入力された値が同じです\n");
-		return 0;
-	}
-
-	return 0;
-}
-static char Min(const char a, const char b) {
-
-	printf("数字以外は入力できません\n");
-	return 0;
 }
 
-int main() {
+template<>
+char Min<char>(char a, char b) {
+	printf("数字以外は入力できません");
+	return '\n';
+}
 
-	int c = Min(100, 10);
-	float d = Min(100.0f, 10.0f);
-	double e = Min(100.0f, 10.0f);
-	char f = Min('A', 'B');
+int main(void) {
+
+	printf("%d\n", Min(10, 12));
+	printf("%f\n", Min(15.0f, 12.0f));
+	printf("%lf\n", Min(13.0, 12.0));
+	printf("%c", Min('s', 'd'));
 
 	return 0;
 }
