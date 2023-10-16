f9c8bf528b0c3bb0a3327284cb075e463d192e35
k022g0038@g.neec.ac.jp <k022g0038@g.neec.ac.jp>
2023-10-16 10:21:28 +0900

    再帰関数を実装

--- main.cpp
+++ main.cpp
@@ -62,6 +62,7 @@ static int ReCash(int cash, int time) {
 		return ReCash(cash, time - 1);
 	}
 	
+	// 時間がある場合更に働く
 	if (time > 0) {
 		cash = cash * 2 - 50;
 		return ReCash(cash, time - 1);
@@ -77,14 +78,17 @@ int main() {
 	int recursive = 0;
 	int time = 1;
 
+	// 一般的賃金の合計 よりも 再帰的賃金の合計が多くなったらループを抜ける
 	while (nomal >= recursive)
 	{
-
+		//　時給を加算する
 		nomal += 1072;
 		recursive += ReCash(0, time);
 
+		//一時間ごとに現在の賃金合計を出力
 		printf("勤務時間:%d時間 : 一般的な賃金合計[%d]円 再帰的な賃金合計[%d]円 \n", time, nomal, recursive);
 
+		// 労働時間を加算( +1時間 )
 		time++;
 
 	}
