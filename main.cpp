06a408d9fb246487d469bbe5f289f80a8b614914
k022g0038@g.neec.ac.jp <k022g0038@g.neec.ac.jp>
2023-10-16 12:00:44 +0900

    関数ポインタとコールバック関数を実装

--- main.cpp
+++ main.cpp
@@ -5,21 +5,54 @@
 typedef void (*PFunk)(int);
 
 void DiceRoll(int num) {
-	// n面ダイスの結果を表示
-	printf("%dだよ！\n", rand() % num);
+
+	// ダイス結果を設定
+	int diceNum = rand() % 2;
+
+	// 時間を止める
+	Sleep(500);
+	printf("\n結");
+	Sleep(300);
+	printf("果");
+	Sleep(300);
+	printf("は");
+	Sleep(300);
+	printf("・");
+	Sleep(300);
+	printf("・");
+	Sleep(300);
+	printf("・\n\n");
+	Sleep(1500);
+
+	if (diceNum == num) {
+		printf("あたり！！\n");
+	}
+	else
+	{
+		printf("ハズレ！！\n");
+	}
+
 }
 
-void SetTimeOut(PFunk p,int second) {
-	// コールバック関数を呼び出す
-	Sleep(second * 100);
-	p(&second);
+void SetRollAnswer(PFunk p) {
+	// 回答を入力
+	printf("丁か半か選んでください\n");
+	printf("丁は0、半は1で入力してください\n\n");
+
+	int num = 0;
+	scanf_s("%d", &num);
+
+	p(num);
+
 }
 
 int main() {
+
+	//乱数をリセットする
+	srand(time(nullptr));
 	// 関数ポインタを宣言
-	PFunk p;
-	p = DiceRoll(6);
-	SetTimeOut(p, 10);
+	PFunk p = DiceRoll;
+	SetRollAnswer(p);
 
 	return 0;
 }
