fc44443e293cbb2ff0ed03b997acbe5e5f7e615a
k022g0038@g.neec.ac.jp <k022g0038@g.neec.ac.jp>
2023-10-16 12:39:08 +0900

    01_03の内容をfunctionとラムダ式に置き換え

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <Windows.h>
 #include <random>
+#include <functional>
 
 typedef void (*PFunk)(int);
 
@@ -34,7 +35,7 @@ void DiceRoll(int num) {
 
 }
 
-void SetRollAnswer(PFunk p) {
+void SetRollAnswer(std::function<void(int)> p) {
 	// 回答を入力
 	printf("丁か半か選んでください\n");
 	printf("丁は0、半は1で入力してください\n\n");
@@ -51,8 +52,36 @@ int main() {
 	//乱数をリセットする
 	srand(time(nullptr));
 	// 関数ポインタを宣言
-	PFunk p = DiceRoll;
+	std::function<void(int)> p = [](int num) {// ダイス結果を設定
+		int diceNum = rand() % 2;
+
+		// 時間を止める
+		Sleep(500);
+		printf("\n結");
+		Sleep(300);
+		printf("果");
+		Sleep(300);
+		printf("は");
+		Sleep(300);
+		printf("・");
+		Sleep(300);
+		printf("・");
+		Sleep(300);
+		printf("・\n\n");
+		Sleep(1500);
+
+		if (diceNum == num) {
+			printf("あたり！！\n");
+		}
+		else
+		{
+			printf("ハズレ！！\n");
+		}
+	};
+
 	SetRollAnswer(p);
+
+
 
 	return 0;
 }
