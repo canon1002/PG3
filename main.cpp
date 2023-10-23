5d286f0cf774de002f175c5cbfa060e0301f4714
k022g0038@g.neec.ac.jp <k022g0038@g.neec.ac.jp>
2023-10-23 11:25:45 +0900

    関数ポインタを用いたEnemyクラスを作成

--- main.cpp
+++ main.cpp
@@ -2,86 +2,17 @@
 #include <Windows.h>
 #include <random>
 #include <functional>
-
-typedef void (*PFunk)(int);
-
-void DiceRoll(int num) {
-
-	// ダイス結果を設定
-	int diceNum = rand() % 2;
-
-	// 時間を止める
-	Sleep(500);
-	printf("\n結");
-	Sleep(300);
-	printf("果");
-	Sleep(300);
-	printf("は");
-	Sleep(300);
-	printf("・");
-	Sleep(300);
-	printf("・");
-	Sleep(300);
-	printf("・\n\n");
-	Sleep(1500);
-
-	if (diceNum == num) {
-		printf("あたり！！\n");
-	}
-	else
-	{
-		printf("ハズレ！！\n");
-	}
-
-}
-
-void SetRollAnswer(std::function<void(int)> p) {
-	// 回答を入力
-	printf("丁か半か選んでください\n");
-	printf("丁は0、半は1で入力してください\n\n");
-
-	int num = 0;
-	scanf_s("%d", &num);
-
-	p(num);
-
-}
+#include <memory>
+#include "Enemy.h"
 
 int main() {
 
-	//乱数をリセットする
-	srand(time(nullptr));
-	// 関数ポインタを宣言
-	std::function<void(int)> p = [](int num) {// ダイス結果を設定
-		int diceNum = rand() % 2;
+	// 敵の宣言
+	std::unique_ptr<Enemy> enemy = std::make_unique<Enemy>();
 
-		// 時間を止める
-		Sleep(500);
-		printf("\n結");
-		Sleep(300);
-		printf("果");
-		Sleep(300);
-		printf("は");
-		Sleep(300);
-		printf("・");
-		Sleep(300);
-		printf("・");
-		Sleep(300);
-		printf("・\n\n");
-		Sleep(1500);
-
-		if (diceNum == num) {
-			printf("あたり！！\n");
-		}
-		else
-		{
-			printf("ハズレ！！\n");
-		}
-	};
-
-	SetRollAnswer(p);
-
-
+	while (enemy->GetState() != EnemyState::kEnd) {
+		enemy->Update();
+	}
 
 	return 0;
 }
