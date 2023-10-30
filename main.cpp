b867c1d566b5c923d50ec28e6ebb4f9524f041e7
k022g0038@g.neec.ac.jp <k022g0038@g.neec.ac.jp>
2023-10-30 10:16:50 +0900

    Min関数の6つのパターンを実装

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <iostream>
 #include <Windows.h>
 #include <random>
 #include <functional>
@@ -12,11 +13,20 @@ int main() {
 	// float
 	std::unique_ptr<Calc<float>> b2 = std::make_unique<Calc<float>>(0.0012f, 0.04003f);
 	// double
-	std::unique_ptr<Calc<double>> b3 = std::make_unique<Calc<double>>(2.003367, 1.41421456);
+	std::unique_ptr<Calc<double>> b3 = std::make_unique<Calc<double>>(2.003367, 1.4142);
+	// int + float
+	std::unique_ptr<Calc<float>> c1 = std::make_unique<Calc<float>>(54, 7.5f);
+	// int + double 
+	std::unique_ptr<Calc<double>> c2 = std::make_unique<Calc<double>>(4371, 0.1753f);
+	// float + double
+	std::unique_ptr<Calc<double>> c3 = std::make_unique<Calc<double>>(6.0893f, 99.414);
 
-	printf("2つの数値の小さい方の値は%d\n", b1->Min());
-	printf("2つの数値の小さい方の値は%f\n", b2->Min());
-	printf("2つの数値の小さい方の値は%lf\n", b3->Min());
-
-	return 0;
+	printf("<int,int>2つの数値の小さい方の値は%d\n", b1->Min());
+	printf("<float,float>2つの数値の小さい方の値は%lf\n", b2->Min());
+	printf("<double,double>2つの数値の小さい方の値は%lf\n", b3->Min());
+	printf("<int,float>2つの数値の小さい方の値は%f\n", c1->Min());
+	printf("<int,double>2つの数値の小さい方の値は%lf\n", c2->Min());
+	printf("<float,double>2つの数値の小さい方の値は%lf\n", c3->Min());
+	
+		return 0;
 }
