b9b9abd154f9f3d1a0ec4f410cf0b12ea707364e
k022g0038@g.neec.ac.jp <k022g0038@g.neec.ac.jp>
2023-11-13 12:27:28 +0900

    Typeを2つ使用するように変更 また、不要なファイルを削除

--- Calc.h
+++ Calc.h
@@ -1,25 +1,25 @@
 #pragma once
-template <typename Type>
+template <typename Type, typename Type2>
 
 class Calc
 {
 public: // メンバ関数
 	
 	// コンストラクタ
-	Calc(Type a, Type b) :a_(a), b_(b) {};
+	Calc(Type a, Type2 b) :a_(a), b_(b) {};
 	Type Min() {
 		if (a_ <= b_) {
 			return static_cast<Type>(a_);
 
 		}else {
-			return static_cast<Type>(b_);
+			return static_cast<Type2>(b_);
 		}
 	}
 
 public:	// メンバ変数
 
 	Type a_;
-	Type b_;
+	Type2 b_;
 
 private:
 
--- main.cpp
+++ main.cpp
@@ -9,17 +9,17 @@
 int main() {
 
 	// int
-	std::unique_ptr<Calc<int>> b1 = std::make_unique<Calc<int>>(4, 7);
+	std::unique_ptr<Calc<int,int>> b1 = std::make_unique<Calc<int,int>>(4, 7);
 	// float
-	std::unique_ptr<Calc<float>> b2 = std::make_unique<Calc<float>>(0.0012f, 0.04003f);
+	std::unique_ptr<Calc<float,float>> b2 = std::make_unique<Calc<float,float>>(0.0012f, 0.04003f);
 	// double
-	std::unique_ptr<Calc<double>> b3 = std::make_unique<Calc<double>>(2.003367, 1.4142);
+	std::unique_ptr<Calc<double,double>> b3 = std::make_unique<Calc<double,double>>(2.003367, 1.4142);
 	// int + float
-	std::unique_ptr<Calc<float>> c1 = std::make_unique<Calc<float>>(54, 7.5f);
+	std::unique_ptr<Calc<int,float>> c1 = std::make_unique<Calc<int,float>>(54, 7.5f);
 	// int + double 
-	std::unique_ptr<Calc<double>> c2 = std::make_unique<Calc<double>>(4371, 0.1753f);
+	std::unique_ptr<Calc<int,double>> c2 = std::make_unique<Calc<int,double>>(4371, 0.1753f);
 	// float + double
-	std::unique_ptr<Calc<double>> c3 = std::make_unique<Calc<double>>(6.0893f, 99.414);
+	std::unique_ptr<Calc<float,double>> c3 = std::make_unique<Calc<float,double>>(6.0893f, 99.414);
 
 	printf("<int,int>2つの数値の小さい方の値は%d\n", b1->Min());
 	printf("<float,float>2つの数値の小さい方の値は%lf\n", b2->Min());
@@ -28,5 +28,5 @@ int main() {
 	printf("<int,double>2つの数値の小さい方の値は%lf\n", c2->Min());
 	printf("<float,double>2つの数値の小さい方の値は%lf\n", c3->Min());
 	
-		return 0;
+	return 0;
 }
