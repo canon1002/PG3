c1f8987ba64d8feed8b28897a269296a0ef41683
渡辺雄大 <veliano393386@gmail.com>
2023-12-27 16:33:22 +0900

    山手線の駅を表示する関数及び双方向リストを定義

--- main.cpp
+++ main.cpp
@@ -1,65 +1,63 @@
 #include <stdio.h>
+#include <list>
+#include <iostream> 
 
-static int Min(int a, int b) {
-	if (a < b) {
-		printf("%d\n", a);
-		return a;
-	}
-	if (a > b) {
-		printf("%d\n", b);
-		return b;
-	}
-	if (a = b) {
-		printf("蜈･蜉帙＆繧後◆蛟､縺悟酔縺倥〒縺兔n");
-		return 0;
-	}
+// using namespace を用いて　std:: を使わずに宣言などができる
+using namespace std;
 
-	return 0;
-}
-static float Min(float a, float b) {
-	if (a < b) {
-		printf("%f\n", a);
-		return a;
-	}
-	if (a > b) {
-		printf("%f\n", b);
-		return b;
-	}
-	if (a = b) {
-		printf("蜈･蜉帙＆繧後◆蛟､縺悟酔縺倥〒縺兔n");
-		return 0;
+void DisplayList(list<const char*> l) {
+
+	for (list<const char*>::iterator itr = l.begin(); itr != l.end(); ++itr)
+	{
+		// 駅名を表示
+		cout << *itr << endl;
 	}
 
-	return 0;
-}
-static double Min(double a, double b) {
-	if (a < b) {
-		printf("%lf\n", a);
-		return a;
+	// 表示する年代ごとにわかりやすくするために改行しておく
+	cout << endl;
+
+};
+
+int main(){
+
+	// 山手線の駅　1970年のリスト
+	// 山手線_駅　だと長いので略称”JY”を使用
+	list<const char*> JY{
+		"Tokyo","Kanda","Akihabara","Okachimachi","Ueno",
+		"Uguisudani","Nippori","Tabata","Komagome",
+		"Suagamo","Otsuka","Ikebukuro","Mejiro","Takanobaba",
+		"Shin-Okubo","Shinjuku","Yoyogi","Harajuku","Shibuya",
+		"Ebisu","Meguro","Gotanda","Osaki","Shinagawa",
+		"Tamachi","Hamamatsucho","Shimbashi","Yurakucho"
+	};
+
+	// 1970年の駅のリストを表示
+	cout << "[1970年 山手線 駅一覧]" << endl;
+	DisplayList(JY);
+
+	for (list<const char*>::iterator itr = JY.begin(); itr != JY.end(); ++itr)
+	{
+		// 西日暮里駅(1971年開業) を追加
+		// insertで要素を追加しようとするとイテレータの前方に追加されるので
+		// イテレータが田端のときに追加する
+		if (*itr == "Tabata") {
+			JY.insert(itr, "Nish-Nippori");
+		}
 	}
-	if (a > b) {
-		printf("%lf\n",b);
-		return b;
+
+	cout << "[2019年 山手線 駅一覧]" << endl;
+	DisplayList(JY);
+
+	for (list<const char*>::iterator itr = JY.begin(); itr != JY.end(); ++itr)
+	{
+		// 高輪ゲートウェイ駅(2020年開業) を追加
+		if (*itr == "Tamachi") {
+			JY.insert(itr, "Takawa Gateway");
+		}
 	}
-	if (a = b) {
-		printf("蜈･蜉帙＆繧後◆蛟､縺悟酔縺倥〒縺兔n");
-		return 0;
-	}
 
-	return 0;
-}
-static char Min(const char a, const char b) {
-
-	printf("謨ｰ蟄嶺ｻ･螟悶�ｯ蜈･蜉帙〒縺阪∪縺帙ｓ\n");
-	return 0;
-}
-
-int main() {
-
-	int c = Min(100, 10);
-	float d = Min(100.0f, 10.0f);
-	double e = Min(100.0f, 10.0f);
-	char f = Min('A', 'B');
+	cout << "[2022年 山手線 駅一覧]" << endl;
+	DisplayList(JY);
 
 	return 0;
 }
