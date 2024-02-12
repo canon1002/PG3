534d7b6e78ef3b6f242d99fb41a6cd57f3d1cdb2
canon1002 <k022g0038@g.neec.ac.jp>
2024-02-12 16:25:16 +0900

    学籍番号順に並べて配置 完了

--- main.cpp
+++ main.cpp
@@ -1,63 +1,92 @@
 #include <stdio.h>
-#include <list>
+#include <vector>
+#include <string>
 #include <iostream> 
+#include <algorithm>
 
 // using namespace を用いて　std:: を使わずに宣言などができる
 using namespace std;
 
-void DisplayList(list<const char*> l) {
 
-	for (list<const char*>::iterator itr = l.begin(); itr != l.end(); ++itr)
-	{
-		// 駅名を表示
-		cout << *itr << endl;
-	}
+// ピボットを選択する関数
+static int partition(std::vector<std::string>& str, int low, int high) {
 
-	// 表示する年代ごとにわかりやすくするために改行しておく
-	cout << endl;
+	// substrで学籍番号の数値のみを抜き出す
+	int pivot = stoi(str[high].substr(5, 4)); 
+    int i = low - 1;
 
-};
+    for (int j = low; j < high; j++) {
+        if (stoi(str[j].substr(5, 4)) < pivot) {
+            i++;
+            std::swap(str[i], str[j]); 
+        }
+    }
+
+    std::swap(str[i + 1], str[high]);
+    return i + 1;
+}
+
+// ソートを実行する関数
+static void Sort(std::vector<std::string>& str, int low, int high) {
+    if (low < high) {
+        int pi = partition(str, low, high);
+		Sort(str, low, pi - 1); // 左側
+		Sort(str, pi + 1, high); // 右側
+    }
+}
 
 int main(){
 
-	// 山手線の駅　1970年のリスト
-	// 山手線_駅　だと長いので略称”JY”を使用
-	list<const char*> JY{
-		"Tokyo","Kanda","Akihabara","Okachimachi","Ueno",
-		"Uguisudani","Nippori","Tabata","Komagome",
-		"Suagamo","Otsuka","Ikebukuro","Mejiro","Takanobaba",
-		"Shin-Okubo","Shinjuku","Yoyogi","Harajuku","Shibuya",
-		"Ebisu","Meguro","Gotanda","Osaki","Shinagawa",
-		"Tamachi","Hamamatsucho","Shimbashi","Yurakucho"
-	};
+	// std::vectorで宣言
+	std::vector<std::string> str = { "k022g0108@g.neec.ac.jp","k022g0045@g.neec.ac.jp",
+	"k022g0007@g.neec.ac.jp","k022g0015@g.neec.ac.jp","k022g0028@g.neec.ac.jp",
+	"k022g0049@g.neec.ac.jp","k022g0003@g.neec.ac.jp","k022g0044@g.neec.ac.jp",
+	"k022g0072@g.neec.ac.jp","k022g0055@g.neec.ac.jp","k021g1139@g.neec.ac.jp",
+	"k021g1485@g.neec.ac.jp","k021g1162@g.neec.ac.jp","k021g1278@g.neec.ac.jp",
+	"k020g1229@g.neec.ac.jp","k020g1202@g.neec.ac.jp","k020g1215@g.neec.ac.jp",
+	"k020g1416@g.neec.ac.jp","k020g1245@g.neec.ac.jp","k020g1506@g.neec.ac.jp",
+	"k020g1401@g.neec.ac.jp","k022g0089@g.neec.ac.jp","k022g0107@g.neec.ac.jp",
+	"k022g0135@g.neec.ac.jp","k022g0020@g.neec.ac.jp","k022g0046@g.neec.ac.jp",
+	"k022g0126@g.neec.ac.jp","k022g0068@g.neec.ac.jp","k022g1060@g.neec.ac.jp",
+	"k022g0019@g.neec.ac.jp","k022g0013@g.neec.ac.jp","k022g0024@g.neec.ac.jp",
+	"k022g0134@g.neec.ac.jp","k022g0123@g.neec.ac.jp","k022g0030@g.neec.ac.jp",
+	"k022g0053@g.neec.ac.jp","k022g0023@g.neec.ac.jp","k022g0041@g.neec.ac.jp",
+	"k022g0063@g.neec.ac.jp","k022g0059@g.neec.ac.jp","k022g0078@g.neec.ac.jp",
+	"k022g0011@g.neec.ac.jp","k022g0040@g.neec.ac.jp","k022g0088@g.neec.ac.jp",
+	"k022g0099@g.neec.ac.jp","k022g0060@g.neec.ac.jp","k022g0032@g.neec.ac.jp",
+	"k022g0087@g.neec.ac.jp","k022g0016@g.neec.ac.jp","k022g0116@g.neec.ac.jp",
+	"k022g0056@g.neec.ac.jp","k022g0009@g.neec.ac.jp","k022g0034@g.neec.ac.jp",
+	"k022g0062@g.neec.ac.jp","k022g0102@g.neec.ac.jp","k022g0113@g.neec.ac.jp",
+	"k022g0064@g.neec.ac.jp","k022g0080@g.neec.ac.jp","k022g0114@g.neec.ac.jp",
+	"k022g0025@g.neec.ac.jp","k022g0095@g.neec.ac.jp","k022g0121@g.neec.ac.jp",
+	"k022g0021@g.neec.ac.jp","k022g0104@g.neec.ac.jp","k022g0101@g.neec.ac.jp",
+	"k022g0130@g.neec.ac.jp","k022g0036@g.neec.ac.jp","k022g0004@g.neec.ac.jp",
+	"k022g0093@g.neec.ac.jp","k022g0038@g.neec.ac.jp","k022g0069@g.neec.ac.jp",
+	"k022g0029@g.neec.ac.jp","k022g0010@g.neec.ac.jp","k022g0083@g.neec.ac.jp",
+	"k022g0086@g.neec.ac.jp","k022g0129@g.neec.ac.jp","k022g0048@g.neec.ac.jp",
+	"k022g0027@g.neec.ac.jp","k022g0103@g.neec.ac.jp","k022g0117@g.neec.ac.jp",
+	"k022g0085@g.neec.ac.jp","k022g0052@g.neec.ac.jp","k022g0073@g.neec.ac.jp",
+	"k022g0065@g.neec.ac.jp","k022g0091@g.neec.ac.jp","k022g0136@g.neec.ac.jp",
+	"k022g0079@g.neec.ac.jp","k022g0017@g.neec.ac.jp","k022g0097@g.neec.ac.jp",
+	"k022g0077@g.neec.ac.jp","k022g0050@g.neec.ac.jp","k022g0033@g.neec.ac.jp",
+	"k022g0039@g.neec.ac.jp","k022g0118@g.neec.ac.jp","k022g0112@g.neec.ac.jp",
+	"k022g0120@g.neec.ac.jp","k022g0127@g.neec.ac.jp","k021g1464@g.neec.ac.jp",
+	"k022g0137@g.neec.ac.jp","k022g0066@g.neec.ac.jp","k022g0051@g.neec.ac.jp",
+	"k022g0111@g.neec.ac.jp","k022g0094@g.neec.ac.jp","k022g0128@g.neec.ac.jp",
+	"k022g0109@g.neec.ac.jp","k022g0124@g.neec.ac.jp","k022g0084@g.neec.ac.jp",
+	"k022g0082@g.neec.ac.jp","k022g0122@g.neec.ac.jp","k022g0061@g.neec.ac.jp",
+	"k022g0031@g.neec.ac.jp","k022g0076@g.neec.ac.jp","k022g0002@g.neec.ac.jp",
+	"k022g0090@g.neec.ac.jp","k022g0074@g.neec.ac.jp","k022g0012@g.neec.ac.jp",
+	"k022g0001@g.neec.ac.jp" };
 
-	// 1970年の駅のリストを表示
-	cout << "[1970年 山手線 駅一覧]" << endl;
-	DisplayList(JY);
+	// 並べ替える
+	Sort(str, 0, (int)str.size() - 1);
 
-	for (list<const char*>::iterator itr = JY.begin(); itr != JY.end(); ++itr)
-	{
-		// 西日暮里駅(1971年開業) を追加
-		// insertで要素を追加しようとするとイテレータの前方に追加されるので
-		// イテレータが田端のときに追加する
-		if (*itr == "Tabata") {
-			JY.insert(itr, "Nish-Nippori");
-		}
+	// 文字列の表示
+	for (int i = 0; i < str.size(); i++) {
+		std::cout << str[i] << std::endl;
 	}
-
-	cout << "[2019年 山手線 駅一覧]" << endl;
-	DisplayList(JY);
-
-	for (list<const char*>::iterator itr = JY.begin(); itr != JY.end(); ++itr)
-	{
-		// 高輪ゲートウェイ駅(2020年開業) を追加
-		if (*itr == "Tamachi") {
-			JY.insert(itr, "Takawa Gateway");
-		}
-	}
-
-	cout << "[2022年 山手線 駅一覧]" << endl;
-	DisplayList(JY);
+	
 
 	return 0;
 }
